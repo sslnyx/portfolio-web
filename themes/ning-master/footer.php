10b07adc74ae33df8045020ddf38427d33a600ca
sslnyx <sslnyx@gmail.com>
2018-04-14 02:42:23 -0700

    footer theme screen shoot update

--- themes/ning-master/footer.php
+++ themes/ning-master/footer.php
@@ -11,8 +11,8 @@
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
-				</div><!-- .site-info -->
+
+			</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
 
